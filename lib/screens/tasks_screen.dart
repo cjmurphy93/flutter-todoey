73320cc7d6f91abcd632d4d52ac180415cab01a9
cjmurphy93 <cjmurph93@gmail.com>
2021-07-08 18:00:48 -0400

    styling top of tasksscreen

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -13,9 +13,35 @@ class TasksScreen extends StatelessWidget {
           bottom: 30.0,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Icon(Icons.list),
-            Text('Todoey'),
+            CircleAvatar(
+              child: Icon(
+                Icons.list,
+                size: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+              backgroundColor: Colors.white,
+              radius: 30.0,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Todoey',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              '12 Tasks',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
           ],
         ),
       ),
